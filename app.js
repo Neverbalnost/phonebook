082083842829a248bb1dfdf84f418294974dbaaf
Lena <neverbalnost@gmail.com>
2016-07-26 02:54:22 +0300

    Save data to Local Storage

--- app.js
+++ app.js
@@ -6,12 +6,14 @@ phBook.controller('phBookCtrl', function($scope) {
             {name: "John", surname: "Lennon", phone: "333-756-75", comment: "You may say I'm a dreamer<br>But I'm not the only one<br>I hope someday you'll join us<br>And the world will be as one"},
             {name: "Amy", surname: "Winehouse", phone: "666-232-37", comment: "We only said goodbye with words<br>I died a hundred times<br>You go back to her<br>And I go back to black"},
             {name: "Janis", surname: "Joplin", phone: "643-212-18", comment: "Didn't I make you feel like you were the only man - yeah!<br>An' didn't I give you nearly everything that a woman possibly can?<br>Honey, you know I did!"},
-            {name: "Mick", surname: "Jagger", phone: "123-243-76", comment: "God gave me everything I want<br>Come on<br> I'll give it all to you"},
+            {name: "Mick", surname: "Jagger", phone: "123-243-76", comment: "And the rain fell down<br>On the cold hard ground<br>And the phone kept ringing<br>And we made sweet love"},
             {name: "Marianne", surname: "Faithfull", phone: "435-434-12", comment: "It is the evening of the day<br>I sit and watch the children play<br>Smiling faces I can see<br>But not for me<br>I sit and watch as tears go by"},
-    ]    
+    ] 
     
     $scope.getList = function() {
-        return list;
+        if (localStorage.list) {
+            return JSON.parse(localStorage.list);
+        } else return list;
     }
             
     $scope.remove = function(array, index){
@@ -43,7 +45,7 @@ phBook.controller('phBookCtrl', function($scope) {
             phone: $('#phone').val(), 
             comment: $('#comment').val()
         }
-        
+        localStorage.setItem('list', JSON.stringify($scope.list));
     }
     
     
